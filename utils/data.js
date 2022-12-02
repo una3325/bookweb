f470d8c9ef28a39dc93eea03596ca9864f1f3e35
Yang yuna <a33256448@gmail.com>
2022-12-02 16:30:14 +0900

    7

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs'
 const data = {
+  users: [
+    {
+      name: 'John',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       name: '칵테일, 러브, 좀비',
