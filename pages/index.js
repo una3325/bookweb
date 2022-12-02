7c7db9ac7fbd4c8cf20a035e7689d232870a919f
Yang yuna <a33256448@gmail.com>
2022-12-02 15:55:38 +0900

    6

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import data from '../utils/data'
 export default function Home() {
   return (
     <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 ">
         {data.products.map((product) => (
           <ProductItem product={product} key={product.slug}></ProductItem>
         ))}
