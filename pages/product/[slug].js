ef3318bb47ecc75c855350e77f1f1280bd2dac62
Yang yuna <a33256448@gmail.com>
2022-12-09 17:50:55 +0900

    17

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import Layout from '../../components/Layout'
 import data from '../../utils/data'
+
 export default function ProductScreen() {
   const { query } = useRouter()
   const { slug } = query
@@ -14,29 +15,27 @@ export default function ProductScreen() {
   return (
     <Layout title={product.name}>
       <div className="py-2">
-        <Link href="/">Back to products</Link>
+        <Link href="/">첫 화면으로 돌아가기</Link>
       </div>
+
       <div className="grid md:grid-cols-4 md:gap-3">
         <div className="md:col-span-2">
           <Image
             src={product.image}
             alt={product.name}
             width={640}
-            height={640}
+            height={900}
             layout="responsive"
           ></Image>
         </div>
+
         <div>
           <ul>
             <li>
-              <h1 className="text-lg">{product.name}</h1>
+              <h1 className="text-lg ">{product.name}</h1>
             </li>
-            <li>Category: {product.category}</li>
-            <li>Brand: {product.brand}</li>
-            <li>
-              {product.rating} of {product.numReviews} reviews
-            </li>
-            <li>Description: {product.description}</li>
+            <li>카테고리: {product.category}</li>
+            <li>출판사: {product.brand}</li>
           </ul>
         </div>
 
@@ -46,10 +45,8 @@ export default function ProductScreen() {
               <div>Price</div>
               <div>{product.price}원</div>
             </div>
-            <div className="mb-2 flex justify-between">
-              <div>Status</div>
-              <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
-            </div>
+
+            <div className="mb-2 flex justify-between"></div>
           </div>
         </div>
       </div>
--- pages/review.js
+++ pages/review.js
@@ -1,14 +1,23 @@
-import Layout from '../components/Layout'
-import ProductItem from '../components/ProductItem'
-import data from '../utils/data'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div className={styles.homeTitle}>
-      <a href="https://client-wheat-alpha.vercel.app/">
-        리뷰를 작성하려면 클릭해주세요.
-      </a>
-    </div>
+    <Layout>
+      <div className="py-2">
+        <Link href="/">첫 화면으로 돌아가기</Link>
+      </div>
+      <div className={styles.homeTitle}>
+        <p>
+          <a href="https://client-gold-mu.vercel.app/">
+            리뷰를 작성하려면 클릭해주세요.
+          </a>
+          <div className={styles.img}>
+            <img src="/images/aaa.jpg" alt="click" width="200px" />
+          </div>
+        </p>
+      </div>
+    </Layout>
   )
 }
